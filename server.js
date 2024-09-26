d8c983bfbf31c9a3c5e1db02d0428e96a8b2cd41
caioxsf <caio.nobile@hotmail.com>
2024-09-26 14:50:22 -0300

    #

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '../../' + '/public'));
+app.use(express.static("public"));
 app.use('/', pedidosRoute);
 
 app.listen(5000, function() {
